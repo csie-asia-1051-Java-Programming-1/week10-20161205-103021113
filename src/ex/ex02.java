b6223e35195d7ee6b44c74fc4e18316b8f36641f
103021113 <103021113@live.asia.edu.tw>
2016-12-11 21:54:39 +0800

    103021113

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -5,11 +5,33 @@
  * Author: 1050210XX 周永振老師
  */
 
+import java.util.Scanner;
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		int x;
+		x=scn.nextInt();
+		if(x<=120){
+			System.out.println(x*2.10);
+			System.out.println(x*2.10);
+			}
+		if(x>120&&x<331){
+			System.out.println(x*3.02);
+			System.out.println(x*2.68);
 	}
-
+		if(x>330&&x<501){
+			System.out.println(x*4.39);
+			System.out.println(x*3.61);
+	}
+		if(x>500&&x<701){
+			System.out.println(x*4.97);
+			System.out.println(x*4.01);
+	}
+		if(x>=701){
+			System.out.println(x*5.63);
+			System.out.println(x*4.50);
+		}
+	}
 }
